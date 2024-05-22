75b9b73c8a56bae6e698eb30794e743108cc1035
Christian Offenheiser <118592069+Coff1199@users.noreply.github.com>
2024-05-22 17:05:41 -0400

    Small change

--- src/components/ClimbingRoute.js
+++ src/components/ClimbingRoute.js
@@ -3,28 +3,31 @@ function ClimbingRoute(props) {
         <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
             <img 
                 class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" 
-                src="/img/erin-lindford.jpg" 
-                alt="Woman's Face" 
+                src="#" 
+                alt="" 
             />
             <div class="text-center space-y-2 sm:text-left">
                 <div class="space-y-0.5">
-                <p class="text-lg text-black font-semibold">
-                    {props.name}
-                </p>
-                <p class="text-slate-500 font-medium">
-                    Grade: {props.grade}
-                </p>
-                <p class="text-slate-500 font-medium">
-                    Setter: {props.setter}
-                </p>
-                <p class="text-slate-500 font-medium">
-                    Number of Flashes: {props.flashes}
-                </p>
-                <p class="text-slate-500 font-medium">
-                    Number of Sends: {props.sends}
-                </p>
+                    <p class="text-lg text-black font-semibold">
+                        {props.name}
+                    </p>
+                    <p class="text-slate-500 font-medium">
+                        Grade: {props.grade}
+                    </p>
+                    <p class="text-slate-500 font-medium">
+                        Setter: {props.setter}
+                    </p>
+                    <p class="text-slate-500 font-medium">
+                        Number of Flashes: {props.flashes}
+                    </p>
+                    <p class="text-slate-500 font-medium">
+                        Number of Sends: {props.sends}
+                    </p>
                 </div>
-                <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+                <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-300 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+                    Edit Route Data
+                </button>
+                <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-300 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
                     Add Data
                 </button>
             </div>

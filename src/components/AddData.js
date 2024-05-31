53b4ff224e2d31f8324ca074b9f5dd809e9c100b
Christian Offenheiser <118592069+Coff1199@users.noreply.github.com>
2024-05-30 20:40:57 -0400

    small changes

--- src/components/AddData.js
+++ src/components/AddData.js
@@ -35,7 +35,7 @@ export default function AddData(props) {
                         setSends('');
                         props.updateData(id, flashes, sends);
                     }}>
-                        <div classNAme="md:flex md:items-center mb-6">
+                        <div className="md:flex md:items-center mb-6">
                             <div className="md:w-1/3">
                                 <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="flashes">
                                     Flashes to add
--- src/components/EditRouteData.js
+++ src/components/EditRouteData.js
@@ -37,7 +37,7 @@ function EditRouteData(props) {
                     setSetter('');
                     props.updateRoute(id, name , grade, setter);
                 }}>
-                    <div classNAme="md:flex md:items-center mb-6">
+                    <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
                             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
                                 Route Name
@@ -55,7 +55,7 @@ function EditRouteData(props) {
                             />
                         </div>
                     </div>
-                    <div classNAme="md:flex md:items-center mb-6">
+                    <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
                         <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="grade">
                             Grade
@@ -73,7 +73,7 @@ function EditRouteData(props) {
                             />
                         </div>
                     </div>
-                    <div classNAme="md:flex md:items-center mb-6">
+                    <div className="md:flex md:items-center mb-6">
                         <div className="md:w-1/3">
                         <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="setter">
                             Setter

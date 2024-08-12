be911bc3f19ccce8d06b0a30bdd3070eb2384930
Christian Offenheiser <118592069+Coff1199@users.noreply.github.com>
2024-08-11 23:01:28 -0400

    Added Post to front-end

--- src/components/ClimbingRoute.js
+++ src/components/ClimbingRoute.js
@@ -1,11 +1,6 @@
 function ClimbingRoute(props) {
     return (
         <div className="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img 
-                className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" 
-                src="#" 
-                alt="" 
-            />
             <div className="text-center space-y-2 sm:text-left">
                 <div className="space-y-0.5">
                     <p className="text-lg text-black font-semibold">
